dc78e20fa9a88a3fcc2473564da973c9aa959cb9
Arghya <arghyadas242004@gmail.com>
2023-06-06 23:13:21 +0530

    Initialisation

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css"
+
+const App = () => {
+    return (
+        <div className="flrow jcen acen h100">
+            <h1>Made with &lt;3 by Arghya Das for you⌀</h1>
+        </div>
+    )
+}
+
+export default App;
